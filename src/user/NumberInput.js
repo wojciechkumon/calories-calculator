c85b071cdf82145d95a03e316ecfdbf53a5d7488
Wojciech Kumoń <wojtas626@gmail.com>
2017-10-30 18:55:24 +0100

    Keyboard dismissing

--- src/user/NumberInput.js
+++ src/user/NumberInput.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Text, TextInput, View} from 'react-native';
+import {Keyboard, Text, TextInput, TouchableWithoutFeedback, View} from 'react-native';
 
 export default class NumberInput extends React.PureComponent {
 
@@ -14,16 +14,18 @@ export default class NumberInput extends React.PureComponent {
   render() {
     const {name, text} = this.props;
     return (
-      <View style={{padding: 10, flexDirection: 'row'}}>
-        <Text style={{flex: 0.2, flexDirection: 'column'}}>{name}</Text>
-        <TextInput
-          style={{flex: 0.8, height: 40}}
-          onChangeText={text => this.handleTextChange(text)}
-          placeholder={name}
-          keyboardType='numeric'
-          value={text}
-        />
-      </View>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={{padding: 10, flexDirection: 'row'}}>
+          <Text style={{flex: 0.2, flexDirection: 'column'}}>{name}</Text>
+          <TextInput
+            style={{flex: 0.8, height: 40}}
+            onChangeText={text => this.handleTextChange(text)}
+            placeholder={name}
+            keyboardType='numeric'
+            value={text}
+          />
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
