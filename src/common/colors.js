50862944e079e3a64abb2ea8e51a11d984f745f9
ewelinarybczynska <e.rybczynska@gmail.com>
2017-12-02 19:11:53 +0100

    daily total calories and macro added to Food View

--- src/common/colors.js
+++ src/common/colors.js
@@ -1,6 +1,7 @@
 export const PINK = '#C2185B';
 export const GREY = '#eeeeee';
 export const WHITE = '#ffffff';
+export const GREEN = '#42A48D';
 
 export const PIE_CHART_PALETTE = [
     {'r':66,'g':164,'b':141},
